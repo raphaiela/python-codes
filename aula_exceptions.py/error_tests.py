37d9efc66e79972b80f2f32176f670de1ae19cdd
Mateus Robers Amaral <robersmateus@gmail.com>
2024-02-25 12:59:04 -0300

    reusable code

--- aula_exceptions.py/error_tests.py
+++ aula_exceptions.py/error_tests.py
@@ -1,14 +1,14 @@
 def main():
-    x = get_int()
+    x = get_int("what's x? ")
     print(f"x is {x}")
 
-def get_int():
+def get_int(prompt):
 
 #define o loop do programa ate que o usuario escreva um numero
     while True:
 
         try:
-            return int(input("what's x? "))
+            return int(input(prompt))
 
         except ValueError:
             print("x is not an integer")
