2d758b43d9f29cf4602b279c494960ab7fd24d63
MATEUS ROBERS <robersmateus@gmail.com>
2024-02-26 11:27:50 -0300

    programa com erro

--- if_aninhado.py
+++ if_aninhado.py
@@ -3,18 +3,21 @@ opção = int(input("Informe uma opção:\n [1] Conta normal \n [2] Conta univer
 conta_normal = saldo = 1000
 conta_universitaria = saldo1 = 1000
 cheque_especial = 200
-
 saque = int(input("Qual o valor do saque? "))
+status = "Sucesso" if saldo >= saque else "Falha"
+status1 = "Uso do cheque especial! " if saldo + cheque_especial >= saque else "Saldo insuficiente! "
+x = print(f"{status} ao realizar o saque! ")
+y = print(f"{status1}")
 
 if opção == 1:
-    if saldo >= saque:
-        print("Realizando saque... ")
-    elif saque <= (saldo + cheque_especial):
-        print("Saque realizado com uso do cheque especial! ")
+    if saque <= (saldo + cheque_especial):
+        y
+    elif saldo >= saque:
+        x
     else:
-        print("Saldo insuficiente! ")
+        x
 elif opção == 2:
     if saldo1 >= saque:
-        print("Realizando saque... ")
+        x
     else:
-        print("Saldo insuficiente! ")
+        x
