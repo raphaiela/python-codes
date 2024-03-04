c380f9b80852684619fb4298177dad579b6c1cc0
mateus-robers-amaral <160862454+mateus-robers-amaral@users.noreply.github.com>
2024-03-04 14:12:51 +0000

    updates

--- cs50_harvard/plates.py
+++ cs50_harvard/plates.py
@@ -1,4 +1,3 @@
-# a funcao main vai rodar o codigo
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
@@ -6,40 +5,34 @@ def main():
     else:
         print("Invalid")
 
-# a funcao is_valid vai checar os requisitos para ser valido
 def is_valid(s):
 
-# REQUISITOS:
-
-    # conter no minimo 2 e no maximo 6 caracteres
     if len(s) < 2 or len(s) > 6:
         return False
 
-    # proibido numeros no comeco
-    # letras no comeco
-    if s[0].isalpha() == False or s[1].isalpha() == False:
+    if not s[0].isalpha():
         return False
 
-    # verifica se o primeiro numero escrito eh 0
+    for x in s:
+        if x in [".", " ", "!", "?"]:
+            return False
+
     i = 0
-    # o elemento i representa cada caractere da lista s
     while i < len(s):
 
-        # essa condicao checa o primeiro numero
-        if s[i].isalpha() == False:
-            # essa subcondicao checa se o primeiro numero eh 0 e retorna falso
+        if not s[i].isalpha():
             if s[i] == "0":
                 return False
-            # senao encerra a checagem do primeiro numero
             else:
                 break
-        # adiciona uma posicao e retorna o loop while (para checar todos os elementos escritos)
         i += 1
 
-    # proibido sinais de pontuacao e espacos
-    for x in s:
-        # se o elemento pertencer a um dos elementos da lista x, retorna falso
-        if x in [".", " ", "!", "?"]:
+    fn = False
+    for c in s[1:]:
+
+        if c.isdigit():
+            fn = True
+        elif not c.isalpha() or fn:
             return False
 
     return True
