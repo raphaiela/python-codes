0b680a34d8bc8fb9673ad6dd0e89e8072e3231f2
mateus-robers-amaral <160862454+mateus-robers-amaral@users.noreply.github.com>
2024-03-25 17:03:52 -0300

    Create exercicio13.py

---
+++ hilario/exercicio13.py
@@ -0,0 +1,25 @@
+nome = int(input('Qual a idade do nadador? '))
+nome > 0
+
+if 5 <= nome <= 7:
+    print('Categoria Infantil A.')
+if 8 <= nome <= 11:
+    print('Categoria Infantil B.')
+elif 12 <= nome <= 13:
+    print('Categoria Juvenil A.')
+elif 14 <= nome <= 17:
+    print('Categoria Juvenil B.')
+elif nome >= 18:
+    print('Categoria Adulto.')nome = int(input('Qual a idade do nadador? '))
+nome > 0
+
+if 5 <= nome <= 7:
+    print('Categoria Infantil A.')
+if 8 <= nome <= 11:
+    print('Categoria Infantil B.')
+elif 12 <= nome <= 13:
+    print('Categoria Juvenil A.')
+elif 14 <= nome <= 17:
+    print('Categoria Juvenil B.')
+elif nome >= 18:
+    print('Categoria Adulto.')
