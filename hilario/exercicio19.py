965c06e91e58920ec75bfc5e2bcd8d77b428f40e
mateus-robers-amaral <160862454+mateus-robers-amaral@users.noreply.github.com>
2024-04-01 17:10:20 -0300

    Create exercicio19.py

---
+++ hilario/exercicio19.py
@@ -0,0 +1,12 @@
+placa = int(input("Digite o último número da placa do seu carro: "))
+
+if placa == 1 or placa == 2:
+    print("Dia do rodízio: Segunda-feira.")
+elif placa == 3 or placa == 4:
+    print("Dia do rodízio: Terça-feira.")
+elif placa == 5 or placa == 6:
+    print("Dia do rodízio: Quarta-feira.")
+elif placa == 7 or placa == 8:
+    print("Dia do rodízio: Quinta-feira.")
+elif placa == 9 or placa == 0:
+    print("Dia do rodízio: Sexta-feira.")
