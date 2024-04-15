d111d4240b118326746e8ad65444b31a0877c412
mateus-robers-amaral <160862454+mateus-robers-amaral@users.noreply.github.com>
2024-04-15 17:10:49 -0300

    Create exercicio23.py

---
+++ hilario/exercicio23.py
@@ -0,0 +1,10 @@
+# for item in [1, 2, 3, 4, 5, 6]:
+#     print(item)
+
+# a = 'Hello World'
+# for item in a:
+#     print(item)
+
+num = int(input('Digite um número: '))
+for i in range(11):
+    print(f'{num} x {i} =', num * i)
