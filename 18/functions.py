791865ba3cc16e5652a7c5ad0916e93395d68a43
Mateus Robers <robersmateus@gmail.com>
2024-04-24 16:34:51 -0300

    aa

--- 18/functions.py
+++ 18/functions.py
@@ -1,6 +1,12 @@
 def telefone():
-    a = int(input("Digite seu número de telefone: "))
-    
+    tel = input("Digite seu número de telefone: ")
+    print(tel)
+    if tel == 9:
+        print("Digite o DDD, por favor.")
+        return True
+    elif tel == 11:
+        print("Telefone enviado com sucesso!")
+        return False
 i = str(input("Olá, como vai? Poderia inserir os valores solicitados? \n---> "))
 if i == "sim":
     telefone()
