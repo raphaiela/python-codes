f573fc0c0f05e0e1be74633b869fa846063f654e
Mateus Robers <robersmateus@gmail.com>
2024-04-24 16:15:56 -0300

    a

---
+++ 18/functions.py
@@ -0,0 +1,9 @@
+def telefone():
+    a = int(input("Digite seu número de telefone: "))
+    
+i = str(input("Olá, como vai? Poderia inserir os valores solicitados? \n---> "))
+if i == "sim":
+    telefone()
+else:
+    print("Obrigado!")
+
